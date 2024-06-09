01e552928ce2428dcd5696d47c832ae8dff07af6
Wahidatuzzahro225 <126538868+Wahidatuzzahro225@users.noreply.github.com>
2024-06-09 21:02:22 +0700

    Add files via upload

---
+++ kalkulatorkonversi/inputdata.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kalkulatorkonversi;
+
+/**
+ *
+ * @author wahidatuzzahrofebriafithrurrahmah
+ */
+class inputdata {
+
+    static void setText(String input_hexadecimal__) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
