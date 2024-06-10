1b69c2a9875806b5bcb8ea7bbe16b172201da22b
Wahidatuzzahro225 <126538868+Wahidatuzzahro225@users.noreply.github.com>
2024-06-10 22:56:04 +0700

    Add files via upload

---
+++ Pertemuan9/MySQLPersonDAO.java
@@ -0,0 +1,24 @@
+/**
+ * File : MySQLPersonDAO.java
+ * Deskripsi : implementasi PersonDAO untuk MySQL
+ */
+
+package Pertemuan9;
+
+import java.sql.*;
+
+public class MySQLPersonDAO implements PersonDAO {
+    public void savePerson(Person person) throws Exception {
+        String name = person.getName();
+        // membuat koneksi, nama db, user, password menyesuaikan
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3290/pbo", "root", "Pebi1234");
+        // kerjakan mysql query
+        String query = "INSERT INTO person(name) VALUES('" + name + "')";
+        System.out.println(query);
+        Statement s = con.createStatement();
+        s.executeUpdate(query);
+        // tutup koneksi database
+        con.close();
+    }
+}
